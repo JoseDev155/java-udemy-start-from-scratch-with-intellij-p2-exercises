46d7d0cf59bf04fbfbb11177357b0fcef863cb18
Jose <jeduramos7001@gmail.com>
2025-08-05 17:08:06 -0600

    Add ProgramandoConStrings/ project

---
+++ ProgramandoConStrings/src/ProgramandoConStrings.java
@@ -0,0 +1,10 @@
+import java.util.Locale;
+
+public class ProgramandoConStrings {
+    public static void main(String[] args) {
+        String archivo="Imagen_generica.png";
+        int i=archivo.lastIndexOf('.');
+        System.out.println(archivo.length());
+        System.out.println(archivo.substring(i+1));
+    }
+}
