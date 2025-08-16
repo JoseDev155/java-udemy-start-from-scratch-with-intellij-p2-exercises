92afdfa4212420c0f3c98510a6f94979984b36db
Jose <jeduramos7001@gmail.com>
2025-08-16 16:54:14 -0600

    Add ProgramandoConOperadores/ project

---
+++ ProgramandoConOperadores/src/ProgramandoConOperadores.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ProgramandoConOperadores {
+    public static void main(String[] args) {
+        int a=18;
+        int b=15;
+        int c=5;
+
+        double promedio=(++a +--b +c)/3d;
+        System.out.println(promedio);
+    }
+}
